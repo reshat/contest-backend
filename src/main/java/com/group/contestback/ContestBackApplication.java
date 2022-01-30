ad187efae6ba5fec44ae2b9e3bbe3d2388856730
reshat <reshat.sultan@yandex.ru>
2022-01-30 14:23:38 +0300

    Initial commit

---
+++ src/main/java/com/group/contestback/ContestBackApplication.java
@@ -0,0 +1,13 @@
+package com.group.contestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContestBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/contestback/ContestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.contestback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContestBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
